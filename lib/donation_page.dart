22490aaf8e5c86edf61a45507da150072cb98774
bmaroti9 <bmaroti9@gmail.com>
2023-10-31 21:10:54 +0100

    finished info page

--- lib/donation_page.dart
+++ lib/donation_page.dart
@@ -6,6 +6,39 @@ import 'package:hihi_haha/ui_helper.dart';
 import 'package:flutter_donation_buttons/flutter_donation_buttons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+final imageText = [
+  'Clear Night',
+  'Partly Cloudy',
+  'Clear Sky',
+  'Overcast',
+  'Haze',
+  'Rain',
+  'Sleet',
+  'Drizzle',
+  'Thunderstorm',
+  'Heavy Snow',
+  'Fog',
+  'Snow',
+  'Heavy Rain',
+  'Cloudy Night'];
+
+final imageLink = [
+  'https://unsplash.com/photos/time-lapse-photography-of-stars-at-nighttime-YvOT1lJ0NPQ',
+  'https://unsplash.com/photos/ocean-under-clouds-Plkff-dVfNM',
+  'https://unsplash.com/photos/white-clouds--qGKIX1Vxtk',
+  'https://unsplash.com/photos/selective-focus-photography-of-gray-clouds-4C6Rp23RjnE',
+  'https://unsplash.com/photos/silhouette-of-trees-and-sea-L-HxY2XlaaY',
+  'https://unsplash.com/photos/water-droplets-on-clear-glass-1YHXFeOYpN0',
+  'https://unsplash.com/photos/snow-covered-trees-and-road-during-daytime-wyM1KmMUSbA',
+  'https://unsplash.com/photos/a-view-of-a-plane-through-a-rain-covered-window-UsYOap7yIMg',
+  'https://unsplash.com/photos/selective-photography-of-white-thunder-nbqlWhOVu6k',
+  'https://unsplash.com/photos/snowy-forest-on-mountainside-during-daytime-t4hA-zCALUQ',
+  'https://unsplash.com/photos/green-trees-on-mountain-under-white-clouds-during-daytime-obQacWYxB1I',
+  'https://unsplash.com/photos/bokeh-photography-of-snows-SH4GNXNj1RA',
+  'https://unsplash.com/photos/dew-drops-on-glass-panel-bWtd1ZyEy6w',
+  'https://unsplash.com/photos/blue-and-white-starry-night-sky-NpF9JLGYfeQ'
+];
+
 class DonationPage extends StatefulWidget {
   final Color color;
   final settings;
@@ -172,24 +205,232 @@ class _InfoPageState extends State<InfoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            toolbarHeight: 65,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(0)
-            ),
-            elevation: 0,
-            leadingWidth: 50,
-            backgroundColor: darken(color, 0.3),
-            title: comfortatext('About', 25),
-            leading:
-            IconButton(
-              onPressed: (){
-                goBack();
-              },
-              icon: const Icon(Icons.arrow_back, color: WHITE,),
-            )
+      backgroundColor: color,
+      appBar: AppBar(
+          toolbarHeight: 65,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0)
+          ),
+          elevation: 0,
+          leadingWidth: 50,
+          backgroundColor: darken(color, 0.3),
+          title: comfortatext('About', 25),
+          leading:
+          IconButton(
+            onPressed: (){
+              goBack();
+            },
+            icon: const Icon(Icons.arrow_back, color: WHITE,),
+          )
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 40),
+                child: Container(
+                  height: 200.0,
+                  width: 200.0,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                          'assets/backdrops/very_clear.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+              comfortatext(
+                  //'Welcome\nto\nMyWorld\nHello\nWorld\n',
+                '"Overmorrow is a beautiful minimalist weather app."',
+                  26),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 10),
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: darken(color),
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top:10, bottom: 10),
+                          child: comfortatext('Feautures:', 20),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            comfortatext('\u2022 accurate weather forecast',22),
+                            comfortatext('\u2022 open source',22),
+                            comfortatext('\u2022 no ads',22),
+                            comfortatext('\u2022 no data collected',22),
+                            comfortatext('\u2022 minimalist design',22),
+                            comfortatext('\u2022 dynamically adapting color scheme',22),
+                            comfortatext('\u2022 languages support',22),
+                            comfortatext('\u2022 place search',22),
+                            comfortatext('\u2022 weather for current location',22),
+                            comfortatext('\u2022 unit swapping',22),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 10),
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      color: darken(color),
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    children: [
+                    Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top:20, bottom: 10),
+                      child: comfortatext('Developed by:', 20),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: comfortatext('Balint',22),
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            elevation: MaterialStateProperty.all<double>(0),
+                            padding: MaterialStateProperty.all(const EdgeInsets.all(10)),
+                            backgroundColor: MaterialStateProperty.all(Colors.orange),
+                            // <-- Button color
+                            overlayColor: MaterialStateProperty.resolveWith<Color?>((states) {
+                              if (states.contains(MaterialState.pressed)) {
+                                return darken(Colors.orange, 0.2);
+                              }
+                              return null; // <-- Splash color
+                            }),
+                          ),
+                          onPressed: () async { await launchEmail(); },
+                          child: Row(
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.only(right: 3),
+                                child: Icon(Icons.mail_outline,color: WHITE,),
+                              ),
+                              comfortatext('contact me', 20, color: WHITE)],
+                          )
+                        )
+                      ],
+                    ),
+                  ),
+                  ]
+                )
+              ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 10),
+                child: Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: darken(color),
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top:20, bottom: 10),
+                            child: comfortatext('Weather data from:', 20),
+                          ),
+                      ),
+                      TextButton(
+                          onPressed: () async { await _launchUrl('https://www.weatherapi.com/'); },
+                          child: comfortatext('www.weatherapi.com', 20, color: Colors.orange)
+                      )
+                  ]
+                ),
+              ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 10),
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      color: darken(color),
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top:20, bottom: 10),
+                            child: comfortatext('Images used:', 20),
+                          ),
+                        ),
+                        ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: imageText.length,
+                          itemExtent: 40,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: TextButton(
+                                  onPressed: () { _launchUrl(imageLink[index]); },
+                                  child: comfortatext(imageText[index], 20,
+                                      color: Colors.orange),
+                                ),
+                              ),
+                            );
+                          },
+                        )
+                      ]
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-      body: const Text('Welcome\nto\nMyWorld\nHello\nWorld\n'),
+      )
     );
+  }
+}
+
+Future<void> launchEmail() async {
+  final Uri _emailLaunchUri = Uri(
+      scheme: 'mailto',
+      path: 'bmaroti9@gmail.com', // Replace with your email address
+      queryParameters: {'subject': 'Feedback or Support Request'} // Optional subject
+  );
+
+  if (await canLaunchUrl(_emailLaunchUri)) {
+    await launchUrl(_emailLaunchUri);
+  } else {
+    throw 'Could not launch email';
+  }
+}
+Future<void> _launchUrl(String url) async {
+  final Uri _url = Uri.parse(url);
+  if (!await launchUrl(_url)) {
+    throw Exception('Could not launch $_url');
   }
 }
